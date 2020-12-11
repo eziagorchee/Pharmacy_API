7132f787a5eaa61b5b8de3183115b6b8fa93b85e
Chiagozie Eziagor <eziagorchy@gmail.com>
2020-12-11 15:55:30 +0100

    Added Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '16136920019f7c51f2099ded856e277da23db39c',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '16136920019f7c51f2099ded856e277da23db39c',
+    ),
+  ),
+);
